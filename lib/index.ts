7b6457743384ecf505c76ce14184ac79cdfb42f2
Ivo Kovacevic <ikovacevic96k@gmail.com>
2022-02-19 21:04:01 +0100

    Replace pipeline callback with promise version ♻️

--- lib/index.ts
+++ lib/index.ts
@@ -1,18 +1,14 @@
 import build from 'pino-abstract-transport';
 import CloudWatchStream, { CloudWatchStreamOptions } from './CloudWatchLogsStream';
 import Chunkify, { ChunkifyOptions } from './Chunkify';
-import { pipeline } from 'stream';
+import { pipeline } from 'stream/promises';
 
 type Options = CloudWatchStreamOptions & ChunkifyOptions;
 
 function createCloudWatchLogsTransport(options: Options) {
   const chunkify = new Chunkify(options);
   const stream = new CloudWatchStream(options);
-  return build(source => {
-    pipeline(source, chunkify, stream, err => {
-      if (err) throw err;
-    });
-  }, {
+  return build(source => pipeline(source, chunkify, stream), {
     close(err: Error, cb: Function) {
       if (err) return cb(err);
       stream.on('close', () => {
